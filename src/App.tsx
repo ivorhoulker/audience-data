ad216d05b510671ec00bc843e5fbe91fe3cf766c
TEST <bob@example.com>
2020-12-19 20:22:55 +0800

    firebase added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
 
+import firebase from "./Firebase";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/analytics";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import "./App.css";
+
+const firestore = firebase.firestore();
+const auth = firebase.auth();
+interface Question {
+  question: string;
+  weighting: number;
+}
 function App() {
+  const [user] = useAuthState(auth);
+  useEffect(() => {
+    if (!user) {
+      auth.signInAnonymously();
+    }
+  }, []);
+  const questionsRef = firestore.collection("questions");
+  const query = questionsRef.orderBy("weighting").limit(9999);
+
+  const [questions] = useCollectionData(query);
+  const [answer, setAnswer] = useState("");
+  console.log(questions);
+  const handleSubmit = () => {};
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {questions &&
+          (questions as Question[]).map((q, i) => {
+            return <div key={i}>{q.question}</div>;
+          })}
       </header>
+
+      <input value={answer} onChange={(e) => setAnswer(e.target.value)}></input>
+      <button onClick={handleSubmit}></button>
     </div>
   );
 }
