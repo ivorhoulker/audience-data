6ee745215fc92d6527ca29668bae07b4c56512be
TEST <bob@example.com>
2020-12-21 20:14:05 +0800

    questions v3 for 3d graph

--- nodeJsonEditor.js
+++ nodeJsonEditor.js
@@ -1,14 +1,43 @@
 const fs = require("fs");
+const { Z_FILTERED } = require("zlib");
 
 let rawdata = fs.readFileSync("src/data/questions.json");
 let data = JSON.parse(rawdata);
 console.log(data);
-const changes = data.map((datum) => {
-  datum.strength = datum.strength * datum.multiplier;
-  delete datum.multiplier;
-  const output = { ...datum, chinese: "" };
-  return output;
-});
+const changes = data
+  .map((datum) => {
+    datum.strength = datum.strength * datum.multiplier;
+    delete datum.multiplier;
+    if (datum.category === "economics" && datum.subcategory === "markets") {
+      datum.strength = datum.strength * -1;
+    } else if (
+      datum.category === "culture" &&
+      datum.subcategory === "freedom"
+    ) {
+      return null;
+    } else if (
+      datum.category === "culture" &&
+      datum.subcategory === "tradition"
+    ) {
+      datum.strength = datum.strength * -1;
+    } else if (
+      datum.category === "government" &&
+      datum.subcageory === "authority"
+    ) {
+      datum.strength = datum.strength * -1;
+    }
+    if (
+      datum.category === "foreign-policy" ||
+      datum.category === "equality" ||
+      datum.category === "personal-freedom"
+    ) {
+      return null;
+    }
+    delete datum.subcategory;
+    const output = { ...datum, chinese: "" };
+    return output;
+  })
+  .filter((x) => !!x);
 
 const toWrite = JSON.stringify(changes, null, 2);
-fs.writeFileSync("src/data/questions-v2.json", toWrite);
+fs.writeFileSync("src/data/questions-v3.json", toWrite);
